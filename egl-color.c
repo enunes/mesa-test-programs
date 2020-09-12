e6a6ff5d716da3006bb76bab6c523b85acdea60d
Erico Nunes <nunes.erico@gmail.com>
2020-09-12 21:25:42 +0200

    egl-color: issue2925 reproducer
    Signed-off-by: Erico Nunes <nunes.erico@gmail.com>

--- egl-color.c
+++ egl-color.c
@@ -85,6 +85,38 @@ void InitGLES(int width, int height)
 
 void Render(void)
 {
+   GLfloat fu0[4] = { 0.007212 , 0.001496 , 0.005935 , 0.004095 };
+   GLfloat fu1[4] = { 0.001449 , 0.001881 , 0.003066 , 0.001799 };
+   GLfloat fu2[4] = { 0.001318 , 0.009491 , 0.001249 , 0.002285 };
+   GLfloat fu3[4] = { 0.001522 , 0.001391 , 0.001383 , 0.002047 };
+   GLfloat fu4[4] = { 0.001550 , 0.001940 , 0.002526 , 0.001737 };
+   GLfloat fu5[4] = { 0.007111 , 0.002221 , 0.006506 , 0.001327 };
+   GLfloat fu6[4] = { 0.004827 , 0.009871 , 0.001033 , 0.007798 };
+   GLfloat fu7[4] = { 0.002773 , 0.001458 , 0.002801 , 0.001134 };
+   GLfloat fu8[4] = { 0.001414 , 0.003156 , 0.001467 , 0.002321 };
+   GLfloat fu9[4] = { 0.001001 , 0.006786 , 0.001329 , 0.003189 };
+
+   GLint u0 = glGetUniformLocation(program, "u0");
+   GLint u1 = glGetUniformLocation(program, "u1");
+   GLint u2 = glGetUniformLocation(program, "u2");
+   GLint u3 = glGetUniformLocation(program, "u3");
+   GLint u4 = glGetUniformLocation(program, "u4");
+   GLint u5 = glGetUniformLocation(program, "u5");
+   GLint u6 = glGetUniformLocation(program, "u6");
+   GLint u7 = glGetUniformLocation(program, "u7");
+   GLint u8 = glGetUniformLocation(program, "u8");
+   GLint u9 = glGetUniformLocation(program, "u9");
+   glUniform4fv(u0, 1, fu0);
+   glUniform4fv(u1, 1, fu1);
+   glUniform4fv(u2, 1, fu2);
+   glUniform4fv(u3, 1, fu3);
+   glUniform4fv(u4, 1, fu4);
+   glUniform4fv(u5, 1, fu5);
+   glUniform4fv(u6, 1, fu6);
+   glUniform4fv(u7, 1, fu7);
+   glUniform4fv(u8, 1, fu8);
+   glUniform4fv(u9, 1, fu9);
+
 	GLfloat vertex[] = {
 		-1, -1, 0,
 		-1, 1, 0,
@@ -118,6 +150,10 @@ void Render(void)
 
 	//glDrawElements(GL_TRIANGLES, sizeof(index)/sizeof(GLuint), GL_UNSIGNED_INT, index);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
+   glEnable(GL_SCISSOR_TEST);
+   glScissor(50, 50, 100, 100);
+   glClear(GL_COLOR_BUFFER_BIT);
+
 
 	assert(glGetError() == GL_NO_ERROR);
 
--- egl-color.vert
+++ egl-color.vert
@@ -3,8 +3,19 @@ attribute vec4 colorIn;
 
 varying vec4 color;
 
+uniform vec4 u0;
+uniform vec4 u1;
+uniform vec4 u2;
+uniform vec4 u3;
+uniform vec4 u4;
+uniform vec4 u5;
+uniform vec4 u6;
+uniform vec4 u7;
+uniform vec4 u8;
+uniform vec4 u9;
+
 void main()
 {
-    color = colorIn;
+    color = colorIn + u0 + u1 + u2 + u3 + u4 + u5 + u6 + u7 + u8 + u9;
     gl_Position = vec4(positionIn, 1);
 }
